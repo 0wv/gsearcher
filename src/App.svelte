12d9a7b41352f89464cd46a549c1a2999f1501d7
0wv <w32w64@gmail.com>
2021-06-06 02:58:57 +0000

    :sparkles: add search history

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,24 @@
 
 	let q;
 
+	function appendToSearchHistory(q) {
+		localStorage.setItem(
+			"searchHistory",
+			JSON.stringify([
+				...JSON.parse(localStorage.getItem("searchHistory")),
+				q,
+			])
+		);
+	}
+
+	function initializeSearchHistory() {
+		if (localStorage.getItem("searchHistory") !== null) {
+			return;
+		}
+
+		localStorage.setItem("searchHistory", JSON.stringify([]));
+	}
+
 	function resetClick() {
 		q = "";
 	}
@@ -13,14 +31,22 @@
 			return;
 		}
 
+		appendToSearchHistory(q);
 		window.location.href = `https://google.com/search?q=${q}`;
 	}
+
+	initializeSearchHistory();
 </script>
 
 <main>
 	<input bind:value={q} type="text" />
 	<button on:click={searchClick}>検索</button>
 	<button on:click={resetClick}>リセット</button>
+	<ul>
+		{#each JSON.parse(localStorage.getItem("searchHistory")) as q}
+			<li>{q}</li>
+		{/each}
+	</ul>
 </main>
 
 <style></style>
